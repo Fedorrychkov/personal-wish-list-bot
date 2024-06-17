aee07e242d97bc62815cad5482d1f28bbf131d4b
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-06-17 22:57:04 +0300

    fix: update run config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import * as express from 'express'
 import * as admin from 'firebase-admin'
+import * as fs from 'fs'
 
 import { AppModule } from './app.module'
+import { isDevelop, isProduction } from './env'
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 
+const getCerts = () => {
+  if (isProduction) {
+    return {
+      key: fs.readFileSync('./private.key'),
+      cert: fs.readFileSync('./certificate.crt'),
+    }
+  }
+
+  return {}
+}
+
+// На данный момент серты настроены только для Stage сервера
+const httpsOptions = getCerts()
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false })
+  const server = express()
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), isDevelop ? undefined : { httpsOptions })
 
   const configService: ConfigService = app.get(ConfigService)
   const projectId = configService.get<string>('FIREBASE_PROJECT_ID')
