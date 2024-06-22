d8b9b6448d0dde38f82b5c42b0fd174aeb81c4f8
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-06-23 00:19:14 +0300

    fix: try to fix date parsing in link

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -4,6 +4,14 @@ export const tryToGetUrlOrEmptyString = (url?: string) => {
   if (typeof url === 'string' && url.match(URL_REGEXP)) {
     const [finalUrl] = url.match(URL_REGEXP)
 
+    try {
+      const result = new Date(finalUrl).getTime()
+
+      if (!Number.isNaN(result)) {
+        return null
+      }
+    } catch {}
+
     return finalUrl || null
   }
 
